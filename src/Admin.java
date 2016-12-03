2f086147bec491dca595211921e82616f8c95668
Leena Chaleunsouk <lchaleunsouk1@student.gsu.edu>
2016-12-03 18:17:34 -0500

    Created child classes: Admin and Customer Admin needs to be able to edit most things Customer needs to have a cart and bookings

---
+++ src/Admin.java
@@ -0,0 +1,14 @@
+/*
+ * Created by: Leena Chaleunsouk
+ * On 12/03/16 @ 6:15
+ */
+
+public class Admin extends User
+{
+
+	public Admin() 
+	{
+		super();
+	}
+	
+}
---
+++ src/Customer.java
@@ -0,0 +1,14 @@
+/*
+ * Created by: Leena Chaleunsouk
+ * On 12/03/16 @ 6:15
+ */
+
+public class Customer extends User
+{
+
+	public Customer() 
+	{
+
+	}
+
+}
